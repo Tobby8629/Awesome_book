463f28618a553e8ad19b55cadd65c29e84972629
tobby8629 <sarhmullah15@gmail.com>
2023-01-15 16:31:29 -0800

    create js to delete from Booklist

---
+++ index.js
@@ -0,0 +1,26 @@
+import BookList from "./modules/Booklist.js";
+import { remove } from "./modules/remove.js";
+
+const all = document.querySelector('.all');
+const form = document.querySelector('#form');
+const titl = document.querySelector('#title');
+const autho = document.querySelector('#author');
+
+const booklist = new BookList;
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    //Add to list of book display
+    all.innerHTML="";
+    booklist.Addbook(autho.value,titl.value,all);
+   
+    //remove from the list 
+    const del = document.querySelectorAll('.remove');
+    del.forEach((del)=>{
+        del.addEventListener('click',()=>{
+            remove(booklist,del);
+        })
+    })
+
+})
+
